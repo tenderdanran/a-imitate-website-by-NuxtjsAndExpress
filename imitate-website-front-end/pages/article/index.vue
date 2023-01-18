e6480e3149d4ff9048b2df60384d3aaf15880dee
TenderFlow <1934185949@qq.com>
2023-01-18 15:13:43 +0800

    测试提交操作

--- imitate-website-front-end/pages/article/index.vue
+++ imitate-website-front-end/pages/article/index.vue
@@ -1,14 +1,19 @@
 <!--
  * @Author: TenderFlow
  * @Date: 2023-01-17 23:02:31
- * @LastEditTime: 2023-01-17 23:02:55
+ * @LastEditTime: 2023-01-18 15:12:07
  * @LastEditors: TenderFlow
- * @FilePath: \a-imitate-website-by-nuxtjs\imitate-website-front-end\pages\article\index.vue
+ * @FilePath: \a-imitate-website-by-nuxtjs-nextjs\imitate-website-front-end\pages\article\index.vue
  * @Description: 
  * 
 -->
 <template>
-  <div>文章主页? 也许是文章详情!</div>
+  <div>文章主页? 也许是文章详情!
+
+    <div class="content">
+      这里是文章内容
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,4 +24,9 @@ export default {
 
 <style>
 
+.content{
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+}
 </style>
